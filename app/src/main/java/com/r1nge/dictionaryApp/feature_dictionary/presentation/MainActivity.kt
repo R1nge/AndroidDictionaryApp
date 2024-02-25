139ae60bd6192668996507c2daf38d0cee6e4a72
R1nge <r1ngegamedev@gmail.com>
2024-02-25 15:24:34 +0300

    Fixes

--- app/src/main/java/com/r1nge/dictionaryApp/feature_dictionary/presentation/MainActivity.kt
+++ app/src/main/java/com/r1nge/dictionaryApp/feature_dictionary/presentation/MainActivity.kt
@@ -13,14 +13,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -51,44 +54,48 @@ class MainActivity : ComponentActivity() {
                     }
                 }
 
-                Scaffold {
-                    Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(16.dp)
-                        ) {
-                            TextField(
-                                value =
-                                viewModel.searchQuery.value,
-                                onValueChange = viewModel::onSearch,
-                                modifier = Modifier.fillMaxWidth(),
-                                placeholder = {
-                                    Text(text = "Search...")
-                                }
-                            )
-                        }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        LazyColumn(modifier = Modifier.fillMaxSize()) {
-                            items(state.wordInfoItems.size) { index ->
-                                val wordInfo = state.wordInfoItems[index]
+                Scaffold(
+                    snackbarHost = { SnackbarHost(snackbarHostState) },
+                    content = {
+                        Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(16.dp)
+                            ) {
+                                TextField(
+                                    value = viewModel.searchQuery.value,
+                                    onValueChange = viewModel::onSearch,
+                                    modifier = Modifier.fillMaxWidth(),
+                                    placeholder = {
+                                        Text(text = "Search...")
+                                    }
+                                )
+                                Spacer(modifier = Modifier.height(16.dp))
+                                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                                    items(state.wordInfoItems.size) { index ->
+                                        val wordInfo = state.wordInfoItems[index]
 
-                                if (index > 0) {
-                                    Spacer(modifier = Modifier.height(8.dp))
-                                }
+                                        if (index > 0) {
+                                            Spacer(modifier = Modifier.height(8.dp))
+                                        }
 
-                                WordInfoItem(wordInfo = wordInfo)
+                                        WordInfoItem(wordInfo = wordInfo)
 
-                                if (index < state.wordInfoItems.size - 1) {
-                                    Divider()
+                                        if (index < state.wordInfoItems.size - 1) {
+                                            Divider()
+                                        }
+                                    }
                                 }
+                            }
+
+                            if (state.isLoading) {
+                                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
                             }
                         }
                     }
-
-                }
+                )
             }
         }
     }
 }
-
